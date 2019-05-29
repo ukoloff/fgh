5f39a829499b17a21d162a91827a648ad0ae1882
Stas Ukolov <ukoloff@gmail.com>
2019-05-29 20:15:13 +0500

    Access to chunks

--- t/ah.js
+++ t/ah.js
@@ -1,9 +1,14 @@
 var j = require('JSONStream')
-var es = require('event-stream')
+// var es = require('event-stream')
 
 var hg = require('./hg')
 
 hg()
 .pipe(j.parse('*'))
+.on('data', dumper)
 // .pipe(es.mapSync(JSON.stringify))
 // .pipe(es.mapSync(console.log))
+
+function dumper(data) {
+  console.log(':', JSON.stringify(data))
+}
